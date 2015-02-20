5d9740eadd587a2f1fd95d3ad0f83c82974181e4
Weijie Gao <gweijie01@gmail.com>
2015-02-20 18:08:30 -0500

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,23 +1,16 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
-preload_app true
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+preload_app!
 
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-end
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    msg  = 'Unicorn worker intercepting TERM and doing nothing. '
-    msg += 'Wait for master to send QUIT'
-    puts msg
-  end
-  wo9289.9
-
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
