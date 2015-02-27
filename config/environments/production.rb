d57c4c59c14a9027b963383e06d8ce0e91657323
Weijie Gao <gweijie01@gmail.com>
2015-02-27 01:05:55 -0500

    keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  config.s3_access_key_id     = ENV['S3_ACCESS_KEY_ID']
-  config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+  config.s3_access_key_id     = ENV['S3_ACCESS_KEY']
+  config.s3_secret_access_key = ENV['S3_SECRET_KEY']
 
 end
