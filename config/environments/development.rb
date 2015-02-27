10fabcc470f462e7081a8e13ecb73d98301d5fcf
Weijie Gao <gweijie01@gmail.com>
2015-02-27 00:49:32 -0500

    keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+  config.s3_access_key_id     = ENV['S3_ACCESS_KEY_ID']
+  config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.s3_access_key_id     = ENV['S3_ACCESS_KEY_ID']
+  config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+
 end
