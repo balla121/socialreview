9d16e6c53271dac8467461ee61a498884fa212ac
Weijie Gao <gweijie01@gmail.com>
2015-02-28 17:03:16 -0500

    removed picture

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,16 +5,10 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validate :picture_size
   validates :rating, presence: true
   validates :subject, presence: true
 
   private
 
-  	#validates the size of an uploaded picture
-  	def picture_size
-  		if picture.size > 5.megabytes
-  			errors.add(:picture, "should be less than 5MB")
-  		end
-  	end
+
 end
