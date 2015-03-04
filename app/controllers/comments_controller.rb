b4962e553c00250fe6594046f00bf8f621febe66
Weijie Gao <gweijie01@gmail.com>
2015-03-04 18:12:23 -0500

    cleaned up vagrant and validation for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+	
   def create
     @micropost = Micropost.find(params[:micropost_id])
     @comment = @micropost.comments.create(params[:comment].permit(:comment))
