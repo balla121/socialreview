d4030a2935375415eee922ce48a6eed32c310a7d
Weijie Gao <gweijie01@gmail.com>
2015-03-04 18:16:32 -0500

    fixed redirect for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
 	
   def create
+  	session[:return_to] ||= request.referer
     @micropost = Micropost.find(params[:micropost_id])
     @comment = @micropost.comments.create(params[:comment].permit(:comment))
     @comment.user_id = session[:user_id]
     @comment.save
-    redirect_to root_url
+    redirect_to session.delete(:return_to)
   end
 
 end
